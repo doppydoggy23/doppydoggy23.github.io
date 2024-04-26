854134a3b61adb7e6bac71ffa48ecd80341b8230
doppydoggy23 <119959094+doppydoggy23@users.noreply.github.com>
2024-04-26 14:40:21 +0100

    test1

---
+++ WIP/audio/minisampler/test1.js
@@ -0,0 +1,30 @@
+"use strict";
+
+function initializeUI() {
+
+/*    document.addEventListener("keydown", (event) => {
+        if (event.repeat) // avoid repeated keystrokes
+            return;
+        keyPressed(event.keyCode);
+        // do something
+      });
+    document.addEventListener("keyup", (event) => {
+        if (event.repeat)
+            return;
+        keyReleased(event.keyCode);
+        // do something
+      });
+*/
+      console.log ("initializeUI()");
+}
+
+function keyPressed(keyCode) {
+    //document.getElementById("bamboosound").play();
+    let soundNode=document.getElementById("bamboosound").cloneNode();
+    //soundNode.volume=volume;
+    soundNode.play();
+}
+
+async function keyReleased(keyCode) {
+    //console.log("keyReleased :"+keyCode)  
+}
