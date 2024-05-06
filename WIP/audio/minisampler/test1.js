920848a9e70cb4494a664b7e98dceb5f0d88c714
doppydoggy23 <119959094+doppydoggy23@users.noreply.github.com>
2024-05-06 16:10:31 +0100

    starting canvas drawing

--- WIP/audio/minisampler/test1.js
+++ WIP/audio/minisampler/test1.js
@@ -15,7 +15,8 @@ function initializeUI() {
         // do something
       });
 */
-      console.log ("initializeUI()");
+      //console.log ("initializeUI()");
+      drawMusicPattern();
 }
 
 function keyPressed(keyCode) {
@@ -34,4 +35,41 @@ function SoundButtonClick(soundNumber) {
     //console.log ("Should Play:"+ "sound"+soundNumber);
     let soundNode=document.getElementById("sound"+soundNumber).cloneNode();
     soundNode.play();
+}
+
+function drawMusicPattern() {
+    let canvas = document.getElementById('myCanvas');
+    let ctx = canvas.getContext("2d");
+    let canvasWidth = canvas.width;
+    let canvasHeight = canvas.height;
+
+
+/*    ctx.resetTransform();
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, 100, 100);*/
+    //ctx.moveTo(100, 100);
+
+    let patternLength=25;
+    let numInstruments=5;
+
+    //
+    let squareSide=Math.floor(canvasWidth/patternLength);
+    for (let ii=0; ii<numInstruments; ii++) {        
+        
+        let ypos=(ii*squareSide)+Math.floor(squareSide/2);
+        for (let i=0; i<patternLength; i++) {
+            let xpos=(i*squareSide)+Math.floor(squareSide/2);            
+            drawPoint(ctx, xpos, ypos, squareSide*0.1);
+        }
+    }
+    //
+
+}
+
+function drawPoint(ctx, x, y, radius) {
+    ctx.fillStyle = "black";
+    ctx.beginPath();    
+    ctx.arc(x, y, radius, 0, 2*Math.PI);
+    ctx.fill();
+
 }
