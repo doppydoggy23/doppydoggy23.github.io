b5026085cad072d884495766a98fa54ab9b99a40
doppydoggy23 <119959094+doppydoggy23@users.noreply.github.com>
2024-09-13 10:49:09 +0100

    testing new UI

--- WIP/audio/minisampler/minisampler2/minisampler2.js
+++ WIP/audio/minisampler/minisampler2/minisampler2.js
@@ -4,33 +4,23 @@ let Globals = {
     numInstruments: 5,
     musicPattern: null, // array that holds the music pattern
     maxMusicPatternLength: 25, // should match or be larger than <sl-range> PatternLengthRange
-    internalTimerFPS: 10, // max value of internalFrames. Should be equal than <SpeedRange> max
+    //internalTimerFPS: 10, // max value of internalFrames. Should be equal than <SpeedRange> max
     //
     currentColumn: 0,
     isPlaying: false,
     timerID: null,
-    internalFrames: 0, // counter that counts how many ticks before playing the next note
+    //internalFrames: 0, // counter that counts how many ticks before playing the next note
+    saveFileName: "MyRythm.json",
+    instrumentButtonSelected: null,
 };
 
-//let howlerSound;
-
 function initializeUI() {
-
-/*    howlerSound = new Howl({
-        src: ['sounds/bells/bell1.mp3']
-      });*/
-      
-      
 
 /*    document.addEventListener("keydown", (event) => {
         if (event.repeat) // avoid repeated keystrokes
             return;
-        keyPressed(event.keyCode);        
+        keyPressed(event.keyCode);
         // do something
-        //let id=howlerSound.play();
-        //howlerSound.rate(0.5);
-        //howlerSound.play();
-        //howlerSound.play();
       });*/
 /*    document.addEventListener("keyup", (event) => {
         if (event.repeat)
@@ -58,9 +48,14 @@ function initializeUI() {
       Globals.musicPattern[2][5]=1;
       Globals.musicPattern[4][24]=1;*/
 
-      //attach to the range controller its change event handler
+      //attach to the track length range controller its change event handler
       document.getElementById("PatternLengthRange").addEventListener('sl-change', event => {
         patternLengthOnChange();
+      });
+
+      //attach to the speed range controller its change event handler
+      document.getElementById("SpeedRange").addEventListener('sl-change', event => {
+        speedRangeOnChange();
       });
 
       drawMusicPattern();
@@ -76,6 +71,7 @@ function keyPressed(keyCode) {
 }
 
 function PlayButtonClick() {
+    Globals.instrumentButtonSelected=null;
     //console.log ("PlayButtonClick()");
     if (Globals.isPlaying)
 	{
@@ -84,14 +80,17 @@ function PlayButtonClick() {
 	}
 	
     Globals.isPlaying=true;
-    Globals.internalFrames=0;
+    //Globals.internalFrames=0;
 	// start from the first frame
     Globals.currentColumn=0;
-	let myTimerID=setInterval(() => {playTimerFunc();}, (1000/Globals.internalTimerFPS));
+    let myInterval=1000-document.getElementById("SpeedRange").value;
+	let myTimerID=setInterval(() => {playTimerFunc();}, myInterval);
 	Globals.timerID=myTimerID;
 }
 
 function StopButtonClick() {
+    Globals.instrumentButtonSelected=null;
+
 	Globals.isPlaying=false;
 	if (Globals.timerID!=null) {
 		clearInterval(Globals.timerID);
@@ -102,15 +101,19 @@ function StopButtonClick() {
 
 function playTimerFunc()
 {
-    // check if counter Globals.internalFrames overflowed as per SpeedRange control
-    let howManyIFWeMustCount=Globals.internalTimerFPS-document.getElementById("SpeedRange").value;
+    soundPatternStep();
+}
 
-    if (Globals.internalFrames>=howManyIFWeMustCount) {
-        soundPatternStep();
-        Globals.internalFrames=0;
-    } else {
-        Globals.internalFrames++;
+function speedRangeOnChange() {
+    Globals.instrumentButtonSelected=null;
+
+	if ((Globals.timerID!=null)&&(Globals.isPlaying==true)) {
+		clearInterval(Globals.timerID);
+        let myInterval=1000-document.getElementById("SpeedRange").value;
+        let myTimerID=setInterval(() => {playTimerFunc();}, myInterval);
+        Globals.timerID=myTimerID;
     }
+
 }
 
 function soundPatternStep() {
@@ -150,6 +153,8 @@ async function keyReleased(keyCode) {
 
 function playSound(sound) {
     let soundNode=document.getElementById(sound).cloneNode();
+    //soundNode.playbackRate.value=2;
+    //soundNode.detune.value=299;
     soundNode.play();
 }
 
@@ -159,9 +164,16 @@ function SoundButtonClick(soundNumber) {
     //console.log ("Should Play:"+ "sound"+soundNumber);
     let soundNode=document.getElementById("sound"+soundNumber).cloneNode();
     soundNode.play();
+
+    //check if user has an instrument selected
+    if (Globals.instrumentButtonSelected!=null) {
+        document.getElementById("Selector"+Globals.instrumentButtonSelected).value="sound"+soundNumber;
+        Globals.instrumentButtonSelected=null;
+    }
 }
 
 function patternLengthOnChange() {
+    Globals.instrumentButtonSelected=null;
     //console.log(document.getElementById("PatternLengthRange").value);
     drawMusicPattern();
 }
@@ -262,4 +274,131 @@ function canvasClick(event) {
     }
 
     drawMusicPattern();
+}
+
+
+function SaveButtonClick() {
+    Globals.instrumentButtonSelected=null;
+
+    //create saving object with all the needed info
+    let savingOBJ= {
+        //mytext: "hello mytext",
+        //sampleArray: [1, 2, 3, 4]
+        magic: "MSFF",
+        instruments: [],
+        pattern:[],
+        patternLength: 0,
+        speed:0,
+    }
+    //save instruments
+    for (let i=0; i<Globals.numInstruments; i++) {
+        savingOBJ.instruments[i]=document.getElementById("Selector"+(i+1)).value;
+    }
+    //save pattern
+    let soP=0;
+    for (let i=0; i<Globals.numInstruments; i++) {
+      for (let x=0; x<Globals.maxMusicPatternLength; x++) {
+        savingOBJ.pattern[soP]=Globals.musicPattern[i][x];
+        soP++;
+      }
+    }
+    //save pattern length and speed
+    savingOBJ.patternLength=document.getElementById("PatternLengthRange").value;
+    savingOBJ.speed=document.getElementById("SpeedRange").value;
+    //console.log ("" + savingOBJ.patternLength+  " "+ savingOBJ.speed);
+
+
+    let content=JSON.stringify(savingOBJ);
+    //let content = 'This is a text'; //original
+  
+    //create a file and put the content, name and type
+    //let file = new File(["\ufeff"+content], 'myFile.txt', {type: "text/plain:charset=UTF-8"}); // original
+    let file = new File([content], Globals.saveFileName, {type: "text/plain:charset=UTF-8"});
+  
+    //create a ObjectURL in order to download the created file
+    let url = window.URL.createObjectURL(file);
+  
+    //create a hidden link and set the href and click it
+    var a = document.createElement("a");
+    a.style = "display: none";
+    a.href = url;
+    a.download = file.name;
+    a.click();
+    window.URL.revokeObjectURL(url);
+
+    //
+    //var obj = {a: 123, b: "4 5 6"};
+    //var data = "text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(obj));
+    //console.log(""+document.getElementById("Selector1").value);
+    //document.getElementById("Selector1").value="sound5";
+}
+
+function LoadButtonClick(event) {
+    Globals.instrumentButtonSelected=null;
+
+    //let file = event.target.files[0];   
+
+/*    let fr=new FileReader();
+        fr.onload=function(){
+        txt.value=fr.result;
+    };
+
+    fr.readAsText(event.target.files[0]);*/
+    document.getElementById('my_file').onchange = function (e) {
+
+        //console.log (""+document.getElementById("my_file").files[0]);
+        var file = e.target.files[0]; 
+
+        // setting up the reader
+        var reader = new FileReader();
+        reader.readAsText(file,'UTF-8');
+
+        reader.onload = readerEvent => {
+            var content = readerEvent.target.result; // this is the content!
+            //console.log( content );
+
+            let loadingOBJ=JSON.parse(content);
+            //console.log (loadingOBJ.magic);
+
+            // set all the saved instruments
+            for (let i=0; i<Globals.numInstruments; i++) {
+                document.getElementById("Selector"+(i+1)).value=loadingOBJ.instruments[i];
+            }
+
+            //recover the pattern
+            let soP=0;
+            for (let i=0; i<Globals.numInstruments; i++) {
+            for (let x=0; x<Globals.maxMusicPatternLength; x++) {
+                Globals.musicPattern[i][x]=loadingOBJ.pattern[soP];
+                soP++;
+                }
+            }
+
+            //load pattern length and speed
+            //console.log ("" + loadingOBJ.patternLength+  " "+ loadingOBJ.speed);
+            document.getElementById("PatternLengthRange").value=loadingOBJ.patternLength;
+            document.getElementById("SpeedRange").value=loadingOBJ.speed;
+
+            drawMusicPattern(); //refresh pattern on screen
+         }
+    }
+
+    document.getElementById('my_file').click();
+
+}
+
+function Instrument1ButtonClick() {
+    Globals.instrumentButtonSelected=1;
+}
+function Instrument2ButtonClick() {
+    Globals.instrumentButtonSelected=2;    
+}
+function Instrument3ButtonClick() {
+    Globals.instrumentButtonSelected=3;    
+}
+function Instrument4ButtonClick() {
+    Globals.instrumentButtonSelected=4;
+}
+function Instrument5ButtonClick() {
+    Globals.instrumentButtonSelected=5;
 }
