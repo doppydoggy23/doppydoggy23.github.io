ad06b9f58bc869010aa370b3614dd02aa7c7bc1f
doppydoggy23 <119959094+doppydoggy23@users.noreply.github.com>
2024-10-02 15:58:38 +0100

    additive synth initial commit

---
+++ WIP/audio/adder/adder.js
@@ -0,0 +1,14 @@
+"use strict";
+
+/*
+This code belongs to pubic domain (2023)
+*/
+
+function initializeUI() {
+
+    
+}
+
+function W2FrequencySliderOnInput(){
+    //console.log("W2FrequencySliderOnInput : "+document.getElementById("W2Range").value);
+}
