a2fddc7e8d47558190ff645e2b5b6d04d1240237
doppydoggy23 <119959094+doppydoggy23@users.noreply.github.com>
2024-10-03 18:37:12 +0100

    added testboxes parsing check

--- WIP/audio/adder/adder.js
+++ WIP/audio/adder/adder.js
@@ -1,8 +1,14 @@
 "use strict";
 
 /*
-This code belongs to pubic domain (2023)
+This code belongs to pubic domain (2024)
 */
+
+const GlobalConstants = {
+    MaxWaveFrequency: 10000,
+    MinWaveFrequency: 0,
+    DefaultWaveFrequency: 440,
+}
 
 function initializeUI() {
 
@@ -10,14 +16,95 @@ function initializeUI() {
         HarmonicOnChangeHandler();
       });*/
 
+    document.getElementById("W1FInput").addEventListener('sl-change', event => {
+        W1FrequencyTextOnChange();
+    })
+    document.getElementById("W2FInput").addEventListener('sl-change', event => {
+        W2FrequencyTextOnChange();
+    })
+    document.getElementById("W3FInput").addEventListener('sl-change', event => {
+        W3FrequencyTextOnChange();
+    })
+
+}
+
+function AjustGUIForValidValues () {
+    //Sinusoid 1
+    let value=parseInt(document.getElementById("W1FInput").value);
+    if (isNaN(value)) {
+        document.getElementById("W1FInput").value=""+GlobalConstants.DefaultWaveFrequency;
+    }
+    if (value<GlobalConstants.MinWaveFrequency) {
+        document.getElementById("W1FInput").value=""+GlobalConstants.MinWaveFrequency;
+    }
+    if (value>GlobalConstants.MaxWaveFrequency) {
+        document.getElementById("W1FInput").value=""+GlobalConstants.MaxWaveFrequency;
+    }
+    
+    //Sinusoid 2
+    value=parseInt(document.getElementById("W2FInput").value);
+    if (isNaN(value)) {
+        document.getElementById("W2FInput").value=""+GlobalConstants.DefaultWaveFrequency;
+    }
+    if (value<GlobalConstants.MinWaveFrequency) {
+        document.getElementById("W2FInput").value=""+GlobalConstants.MinWaveFrequency;
+    }
+    if (value>GlobalConstants.MaxWaveFrequency) {
+        document.getElementById("W2FInput").value=""+GlobalConstants.MaxWaveFrequency;
+    }
+    
+    //Sinusoid 3
+    value=parseInt(document.getElementById("W3FInput").value);
+    if (isNaN(value)) {
+        document.getElementById("W3FInput").value=""+GlobalConstants.DefaultWaveFrequency;
+    }
+    if (value<GlobalConstants.MinWaveFrequency) {
+        document.getElementById("W3FInput").value=""+GlobalConstants.MinWaveFrequency;
+    }
+    if (value>GlobalConstants.MaxWaveFrequency) {
+        document.getElementById("W3FInput").value=""+GlobalConstants.MaxWaveFrequency;
+    }
+    
+}
+
+function W1FrequencySliderOnInput () {
+    document.getElementById("W1FInput").value=document.getElementById("W1FRange").value;
 }
 
 function W2FrequencySliderOnInput(){
     //console.log("W2FrequencySliderOnInput : "+document.getElementById("W2Range").value);
+    document.getElementById("W2FInput").value=document.getElementById("W2FRange").value;
 }
 
 function W3FrequencySliderOnInput(){
     //console.log("W2FrequencySliderOnInput : "+document.getElementById("W2Range").value);
+    document.getElementById("W3FInput").value=document.getElementById("W3FRange").value;
+}
+
+function W1FrequencyTextOnChange() {
+    AjustGUIForValidValues();
+    document.getElementById("W1FRange").value=document.getElementById("W1FInput").value;
+}
+
+function W2FrequencyTextOnChange() {
+    AjustGUIForValidValues();
+    document.getElementById("W2FRange").value=document.getElementById("W2FInput").value;
+}
+
+function W3FrequencyTextOnChange() {
+    AjustGUIForValidValues();
+    document.getElementById("W3FRange").value=document.getElementById("W3FInput").value;
+}
+
+function retrieveParsedValues() {
+    AjustGUIForValidValues(); // ToDo: check if this function takes too much time, in case real time sound becomes laggy
+    let W1F=parseInt(document.getElementById("W1FInput").value);
+    let W2F=parseInt(document.getElementById("W2FInput").value);
+    let W3F=parseInt(document.getElementById("W3FInput").value);
+    return {W1F, W2F, W3F};
+}
+function PlayButtonClick() {
+    console.log (retrieveParsedValues());
 }
 
 function SelectHarmonicsOptionRGOnChange() {
