84f142b657bd0abf33a688d1a2ae3ac3f3f1905b
doppydoggy23 <119959094+doppydoggy23@users.noreply.github.com>
2026-02-11 17:21:42 +0000

    minor changes

--- WIP/audio/samplefilter/samplefilter.js
+++ WIP/audio/samplefilter/samplefilter.js
@@ -1,17 +1,5 @@
 "use strict";
 
-/*let Globals = {
-    numInstruments: 5,
-    musicPattern: null, // array that holds the music pattern
-    maxMusicPatternLength: 25, // should match or be larger than <sl-range> PatternLengthRange
-    internalTimerFPS: 10, // max value of internalFrames. Should be equal than <SpeedRange> max
-    //
-    currentColumn: 0,
-    isPlaying: false,
-    timerID: null,
-    internalFrames: 0, // counter that counts how many ticks before playing the next note
-};*/
-
 let WAVInfo ={ 
 //    sampleRate
 //    numChannels
